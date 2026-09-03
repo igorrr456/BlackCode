31640fc799104f986e4e3c4eb06f811bf261967a
igor <igorc7871@gmail.com>
2026-09-03 16:46:15 -0300

    Chore:Configurando as Interfaces e o Repositorio

---
+++ BlackinCode/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using BlackinCode.Models;
+
+namespace BlackinCode.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Usuario Validar(string email, string senha);
+        void CriarConta(Usuario usuario);
+        void DeletarConta(int id);
+        Usuario BuscarPorId(int id);
+        void Atualizar(Usuario usuario);
+    }
+}
